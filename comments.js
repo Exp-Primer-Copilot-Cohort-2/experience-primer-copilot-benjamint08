807e0636d7e61ef71d930714c91d69a97822abea
ben <61296321+benjamint08@users.noreply.github.com>
2024-09-07 20:39:37 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,25 @@
+git pull// Run: node comments.js
+// Access: http://localhost:3000/
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/comments', (req, res) => {
+  fs.readFile(__dirname + '/comments.json', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let comments = JSON.parse(data);
+      res.json(comments);
+    }
+  });
+});
